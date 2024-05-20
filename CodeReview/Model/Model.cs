b1867bc5535e735827ec084840126b0fd2c3c997
Dave the bear <davebear@betterlifepartners.com>
2024-05-20 10:55:35 -0400

    initial commit

---
+++ CodeReview/Model/Model.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProject.Model.Entity
+{
+    public class Model() { }
+
+    public class Member
+    {
+        public Guid? MemberID { get; set; }
+        public string Gender { get; set; }
+        public DateOnly? BirthDate { get; set; }
+        public string MRN { get; set; }
+        public string DisplayName { get; set; }
+        public string PrimaryPhoneNumber { get; set; }
+        public string Status { get; internal set; }
+
+        public List<ProgramEnrollment> ProgramEnrollments { get; internal set; }
+        public List<MemberMeetingChange> MemberMeetingChanges { get; internal set; }
+    }
+
+    public class MemberMeetingChange
+    {
+        public Guid MemberMeetingChangeID { get; set; }
+        public Guid MemberID { get; set; }
+        public Member Member { get; set; } // Navigation property
+        string DisplayName { get; set; }
+
+        public DateTime ChangeDate {get;set;}
+    }
+
+    public class ProgramEnrollment
+    {
+        public Guid ProgramEnrollmentID { get; set; }
+        public Guid MemberID { get; set; }
+        public Member Member { get; set; } // Navigation property
+        public List<ProgramEnrollmentTrack> ProgramEnrollmentTracks { get;set;}
+        public Guid ProgramID { get; set; }
+        public string ProgramName { get; set; }
+        public string ProgramDescription { get; set; }
+
+    }
+    public class ProgramEnrollmentTrack
+    {
+        public Guid ProgramEnrollmentTrackID { get; set;}
+        public Track Track { get; set; }
+        public Guid TrackID;
+
+    }
+
+    public class Track
+    {
+        public Guid TrackID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ CodeReview/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using InterviewProject.Model;
+using InterviewProject.Model.Entity;
+
+var memberID = Guid.NewGuid();
+var oudProgramID = Guid.NewGuid();
+
+var weeklyTrackID = new Guid("AE96516D-F9BF-4F20-9828-45CB943D1216");
+
+
+var member = new Member()
+{
+    MemberID = memberID,
+    Gender = "Male",
+    BirthDate = new DateOnly(1997, 2, 19),
+    MRN = memberID.ToString().Substring(0,6),
+    DisplayName = "Ralph Bunson",
+    PrimaryPhoneNumber = "(123) 324-123",
+    Status = "Active - Regular",
+};
+
+member.ProgramEnrollments = new List<ProgramEnrollment>
+{
+    new ProgramEnrollment()
+    {
+        ProgramEnrollmentID = Guid.NewGuid(),
+        MemberID = memberID,
+        Member = member,
+        ProgramID = oudProgramID,
+        ProgramName = "OUD",
+        ProgramDescription = "Opoid Use Disorder",
+        ProgramEnrollmentTracks = new List<ProgramEnrollmentTrack>()
+        {
+            new ProgramEnrollmentTrack()
+            {
+                ProgramEnrollmentTrackID = Guid.NewGuid(),
+                Track = new Track()
+                {
+                    TrackID = weeklyTrackID,
+                    Name = "Weekly",
+                }
+            }
+        }
+    }
+};
+
+member.MemberMeetingChanges = new List<MemberMeetingChange>() { };
